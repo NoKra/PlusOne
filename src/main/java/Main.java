33c3b06c01d38ee11b3e754b43cabad59deb5dd1
NoKra <Kraus.N@protonmail.ch>
2023-02-28 20:07:26 -0500

    database testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import database.DbConnect;
 import window_object.WindowObject;
+
+import java.sql.SQLException;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        //run();
+        dbTest();
+
+
+    }
+
+    public static void run() {
         WindowObject mainWindow = new WindowObject();
         mainWindow.changeWindowSize();
 
@@ -11,5 +21,10 @@ public class Main {
 
         mainWindow.setWindowVisible();
         mainWindow.centerWindow();
+    }
+
+    public static void dbTest() throws SQLException {
+        DbConnect newConnect = new DbConnect();
+        newConnect.testConnection();
     }
 }
